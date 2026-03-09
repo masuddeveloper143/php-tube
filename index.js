10b03af7e33a72edc3a18f0532c0824048314641
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-09 22:46:00 +0800

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-console.logI("index is connected");
+console.log("index is connected");
 
 function lodCaregories() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
 
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((data) => displayCategories(data.categories));
 
+};
+
+function displayCategories(categories) {
+    console.log(categories);
 }
+
+lodCaregories();
