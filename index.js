2d751841137bc2c2d3a2def508a68f67ff9d27a9
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-10 01:16:48 +0800

    first commit

--- index.js
+++ index.js
@@ -41,8 +41,30 @@ function displayCategories(categories) {
 const displayVideos = (videos) => {
     const videosContainer = document.getElementById("videos-container");
 
-    videos.forEach(videos => {
-    console.log(videos)
+    videos.forEach(video => {
+        console.log(videos);
+
+        const videoCrad = document.createElement("div");
+        videoCrad.innerHTML = `
+        
+      <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail} "
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title} </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        
+        `;
+
+        videosContainer.append(videoCrad);
 
     });
 
