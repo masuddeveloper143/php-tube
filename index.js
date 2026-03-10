1fb9c81a1aea5c1db1d21809a7afd559843e39a0
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-11 00:17:07 +0800

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function lodCaregories() {
 function loadVideos() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then((Response) => Response.json())
-        .then((data) => displayVideos(data.videos));
+        .then((data) => {
+            removeActiveClass();
+            document.getElementById("btn-all").classList.add("active");
+            displayVideos(data.videos);
+        });
 }
 
 const loadCategoryVideos = (id) => {
